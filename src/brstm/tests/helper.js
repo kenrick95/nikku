c01739d20c1a8125eb638985c273f594e94009c0
Kenrick <kenrick95@gmail.com>
2021-01-13 23:06:35 +0800

    Modify test to check for track description

--- src/brstm/tests/helper.js
+++ src/brstm/tests/helper.js
@@ -2,20 +2,51 @@
 
 const assert = require('assert').strict;
 
+const color = {
+  /**
+   * @param {string} message 
+   */
+  green: (message) => {
+    return '\x1b[32m' + message + '\x1b[0m';
+  },
+  /**
+   * @param {string} message 
+   */
+  red: (message) => {
+    return '\x1b[31m' + message + '\x1b[0m';
+  },
+};
+
+/**
+ *
+ * @param {string} testName
+ * @param {() => void} block
+ */
 function test(testName, block) {
   try {
     block();
-    console.log(`[OK] ${testName}`);
+    console.log(color.green(`[OK]`), `${testName}`);
   } catch (e) {
-    console.error(`[FAIL] ${testName}: ${e.message}`, e);
+    console.error(color.red(`[FAIL]`), `${testName}:\r\n`, e, '\r\n\r\n');
+    require('process').exitCode = 1;
   }
 }
 
+/**
+ * @template T
+ * @param {T} actualValue
+ * @returns {{
+ *  toBe: (expectedValue: T) =>  void,
+ *  toEqual: (expectedValue: T) =>  void,
+ * }}
+ */
 function expect(actualValue) {
   return {
     toBe: (expectedValue) => {
-      // @ts-ignore
       assert.strictEqual(actualValue, expectedValue);
+    },
+    toEqual: (expectedValue) => {
+      assert.deepStrictEqual(actualValue, expectedValue);
     },
   };
 }
