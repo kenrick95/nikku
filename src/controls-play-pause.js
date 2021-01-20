d0dba21d7f019c55fcd442836235266ad8410324
Kenrick <kenrick95@gmail.com>
2021-01-20 23:05:49 +0800

    More things

--- src/controls-play-pause.js
+++ src/controls-play-pause.js
@@ -1,16 +1,23 @@
+//@ts-check
 class ControlsPlayPause extends HTMLElement {
   constructor() {
     super();
+
     this.attachShadow({ mode: 'open' });
     /**
      * - icon: Which icon is showing: play icon or pause icon?
      * @type {{ icon: 'play' | 'pause' }}
      */
     this.state = {
-      icon: this.getAttribute('status') || 'play',
+      icon:
+        /** @type {'play' | 'pause'} */ (this.getAttribute('icon')) || 'play',
     };
     this.init();
   }
+  /**
+   *
+   * @param {string} html
+   */
   parseHTML(html) {
     var t = document.createElement('template');
     t.innerHTML = html;
@@ -18,51 +25,62 @@ class ControlsPlayPause extends HTMLElement {
   }
 
   async init() {
-    this.iconPlay = this.parseHTML(
-      await (await fetch('../assets/play-icon.svg')).text()
-    );
-    this.iconPause = this.parseHTML(
-      await (await fetch('../assets/pause-icon.svg')).text()
-    );
-    this.render();
+    let [iconPlayString, iconPauseString] = await Promise.all([
+      fetch('../assets/play-icon.svg').then((res) => res.text()),
+      fetch('../assets/pause-icon.svg').then((res) => res.text()),
+    ]);
+    this.iconPlay = this.parseHTML(iconPlayString);
+    this.iconPause = this.parseHTML(iconPauseString);
+    this.button = document.createElement('button');
+    this.button.setAttribute('class', 'button');
+
+    const icon = this.state.icon === 'play' ? this.iconPlay : this.iconPause;
+    this.button.appendChild(icon);
+
+    const style = document.createElement('style');
+    style.textContent = this.elementStyle;
+
+    this.shadowRoot.innerHTML = '';
+    this.shadowRoot.append(style, this.button);
   }
 
   static get observedAttributes() {
-    return ['status'];
+    return ['icon'];
   }
+
+  /**
+   *
+   * @param {string} name
+   * @param {string} oldValue
+   * @param {string} newValue
+   */
   attributeChangedCallback(name, oldValue, newValue) {
-    console.log('Attributes changed.', name, oldValue, newValue);
+    this.state[name] = newValue;
+    if (oldValue != null && oldValue != newValue) {
+      if (name === 'icon' && this.button) {
+        const icon =
+          this.state.icon === 'play' ? this.iconPlay : this.iconPause;
+        this.button.replaceChild(icon, this.button.firstChild);
+      }
+    }
   }
 
   get elementStyle() {
     return `
     .button {
-        all: initial;
-        width: 80px;
-        height: 80px;
-        border-radius: 40px;
+      all: initial;
+      width: 80px;
+      height: 80px;
+      border-radius: 40px;
     }
     svg {
-        width: 100%;
-        height: 100%;
+      width: 100%;
+      height: 100%;
     }
     .button:hover {
-        background: var(--primary-lightest-1);
-        box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.24);
+      background: var(--primary-lightest-1);
+      box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.24);
     }`;
-  }
-
-  render() {
-    const button = document.createElement('button');
-    button.setAttribute('class', 'button');
-
-    const icon = this.state.icon === 'play' ? this.iconPlay : this.iconPause;
-    button.appendChild(icon);
-
-    const style = document.createElement('style');
-    style.textContent = this.elementStyle;
-
-    this.shadowRoot.append(style, button);
   }
 }
 customElements.define('controls-play-pause', ControlsPlayPause);
