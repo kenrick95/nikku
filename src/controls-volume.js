efa0693de60ee7d1ddb98e6a8bea944e82376e35
Kenrick <kenrick95@gmail.com>
2021-04-27 23:11:28 +0800

    Implement draggable volume indicator

--- src/controls-volume.js
+++ src/controls-volume.js
@@ -15,19 +15,23 @@ class ControlsVolume extends HTMLElement {
       volume: parseFloat(this.getAttribute('volume') || '1.0'),
     };
 
-    this._volumeContainerWidth = undefined;
+    this._isDragging = false;
+    /** @type {HTMLDivElement | undefined} */
+    this.volumeContainer = undefined;
+    /** @type {SVGElement | undefined} */
+    this.iconVolume = undefined;
+
+    /** @type {HTMLDivElement | undefined} */
+    this.volumeFill = undefined;
+
+    /** @type {HTMLDivElement | undefined} */
+    this.volumeIndicator = undefined;
+
     this.init();
   }
 
-  get volumeContainerWidth() {
-    if (this._volumeContainerWidth) {
-      return this._volumeContainerWidth;
-    }
-    return (this._volumeContainerWidth =
-      this.shadowRoot?.querySelector('.volume-container')?.clientWidth ?? 0);
-  }
-
   async init() {
+    this._isDragging = false;
     let volumeElement = /** @type {DocumentFragment} */ (
       /** @type {HTMLTemplateElement} */ (document.getElementById(
         'template-volume'
@@ -37,27 +41,113 @@ class ControlsVolume extends HTMLElement {
       await fetch('../assets/volume-icon.svg').then((res) => res.text())
     ));
     this.iconVolume?.classList.add('volume-icon');
-    volumeElement.querySelector('.volume-container')?.prepend(this.iconVolume);
+    this.volumeContainer = /** @type {HTMLDivElement} */ (volumeElement.querySelector(
+      '.volume-container'
+    ));
+    this.volumeIndicator = /** @type {HTMLDivElement} */ (volumeElement.querySelector(
+      '.volume-indicator'
+    ));
+    this.volumeFill = /** @type {HTMLDivElement} */ (volumeElement.querySelector(
+      '.volume-fill'
+    ));
+    const volumeBar = /** @type {HTMLDivElement} */ (volumeElement.querySelector(
+      '.volume-bar'
+    ));
 
-    volumeElement
-      .querySelector('.volume-bar')
-      .addEventListener('click', (e) => {
-        // TODO: Not just click :thinking: need pointerdown, pointermove, pointerleave 
-      });
+    this.volumeContainer?.prepend(/** @type {SVGElement} */ (this.iconVolume));
+
+    // Cache the values because they cause page reflow
+    /** @type {number | null} */
+    let cachedVolumeBarOffsetLeft = null;
+    /** @type {number | null} */
+    let cachedVolumeBarClientWidth = null;
+
+    /**
+     *
+     * @param {MouseEvent} e
+     */
+    const updateVolumeFromEvent = (e) => {
+      if (!cachedVolumeBarOffsetLeft) {
+        cachedVolumeBarOffsetLeft = volumeBar?.offsetLeft ?? 0;
+      }
+      if (!cachedVolumeBarClientWidth || cachedVolumeBarClientWidth === 1) {
+        cachedVolumeBarClientWidth = volumeBar?.clientWidth ?? 1;
+      }
+      this.updateStateVolume(
+        Math.min(
+          1,
+          Math.max(
+            0,
+            (e.clientX - cachedVolumeBarOffsetLeft) / cachedVolumeBarClientWidth
+          )
+        )
+      );
+    };
+
+    volumeBar?.addEventListener(
+      'mousedown',
+      (e) => {
+        this._isDragging = true;
+        updateVolumeFromEvent(/** @type {MouseEvent} */ (e));
+      },
+      { passive: true }
+    );
+    volumeBar?.addEventListener(
+      'mousemove',
+      (e) => {
+        if (this._isDragging) {
+          updateVolumeFromEvent(/** @type {MouseEvent} */ (e));
+        }
+      },
+      { passive: true }
+    );
+    document?.addEventListener(
+      'mouseup',
+      (_e) => {
+        this._isDragging = false;
+      },
+      { passive: true }
+    );
+
+    // Invalidate cached values because they have changed
+    window.addEventListener('resize', () => {
+      cachedVolumeBarOffsetLeft = null;
+      cachedVolumeBarClientWidth = null;
+    });
+
+    // @ts-ignore
     this.shadowRoot.innerHTML = '';
-    this.shadowRoot.append(volumeElement);
+    this.shadowRoot?.append(volumeElement);
+
+    // Force reflow
+    this.volumeContainer.clientWidth;
+    this.updateStateVolume(this.state.volume);
   }
 
   static get observedAttributes() {
     return ['muted', 'volume'];
   }
 
-  updateStateMode() {
-    // no-op, unsupported
+  /**
+   *
+   * @param {boolean} newValue
+   */
+  updateStateMode(newValue) {
+    this.state.muted = newValue;
   }
 
+  /**
+   * @param {number} newValue
+   */
   updateStateVolume(newValue) {
-    console.log('newValue', newValue)
+    this.state.volume = newValue;
+
+    if (this.volumeFill) {
+      this.volumeFill.style.width = `${newValue * 100}%`;
+    }
+    if (this.volumeIndicator) {
+      this.volumeIndicator.style.left = `calc(${newValue * 100}% - 5px)`;
+    }
   }
 
   /**
@@ -67,12 +157,11 @@ class ControlsVolume extends HTMLElement {
    * @param {string} newValue
    */
   attributeChangedCallback(name, oldValue, newValue) {
-    this.state[name] = newValue;
     if (oldValue != null && oldValue != newValue) {
       if (name === 'muted') {
-        this.updateStateMode();
+        this.updateStateMode(!!newValue);
       } else if (name === 'volume') {
-        this.updateStateVolume(newValue);
+        this.updateStateVolume(parseFloat(newValue));
       }
     }
   }
