e53a30414cd5577a1c848c12e72299c02b39169c
Kenrick <kenrick95@gmail.com>
2021-05-19 11:43:59 +0800

    Apply disabled UIs

--- src/controls-play-pause.js
+++ src/controls-play-pause.js
@@ -120,7 +120,7 @@ export class ControlsPlayPause extends HTMLElement {
       width: 100%;
       height: 100%;
     }
-    .button:hover {
+    .button:not(.disabled):hover {
       background: var(--primary-lightest-1);
       box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.24);
       cursor: pointer;
