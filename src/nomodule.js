73c7773905969f9199981ebed408f0211480dd12
Kenrick <kenrick95@gmail.com>
2021-05-21 10:57:51 +0800

    Handle error and unsupported browser message

---
+++ src/nomodule.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function () {
+  // @ts-ignore
+  document.getElementById('unsupported').style.display = 'block';
+  // @ts-ignore
+  document.getElementById('main').style.display = 'none';
+});
