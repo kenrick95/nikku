0f6d7b8899ecafb68c761555701ff3ce92c607b1
Kenrick <kenrick95@gmail.com>
2022-03-24 21:20:10 +0800

    feat(tracks): rewrite in lit

--- app/src/controls/controls-tracks.ts
+++ app/src/controls/controls-tracks.ts
@@ -1,152 +1,96 @@
+import { html, css, LitElement } from 'lit';
+import { customElement, property } from 'lit/decorators.js';
+import { classMap } from 'lit/directives/class-map.js';
 
-/**
- * Attributes
- * - "active" {string} comma-separated index of active tracks (0-based)
- * - "count" {string} number of tracks
- * - "disabled" {string} if attribute is present, the UI is not interactable
- */
-export class ControlsTracks extends HTMLElement {
-  state: { active: boolean[]; count: number; disabled: boolean };
-  constructor() {
-    super();
-    this.attachShadow({ mode: 'open' });
+@customElement('controls-tracks')
+export class ControlsTracks extends LitElement {
+  /** JSON-Array of active tracks */
+  @property({ type: Array }) active: Array<boolean> = [];
+  /** number of tracks */
+  @property({ type: Number }) count: number = 0;
+  /** Whether or not UI is interactable */
+  @property({ type: Boolean }) disabled: boolean = false;
 
-    const count = parseInt(this.getAttribute('count') || '0', 10) || 0;
-    this.state = {
-      active: this.parseActiveAttribute(
-        this.getAttribute('active') || '0',
-        count
-      ),
-      count: count,
-      disabled: this.getAttribute('disabled') != null,
-    };
-
-    this.init();
-  }
-
-  /**
-   *
-   * @param {string} activeAttribute
-   * @param {number} count
-   * @returns {Array<boolean>}
-   */
-  parseActiveAttribute(activeAttribute: string, count: number): boolean[] {
-    /** @type {Array<boolean>} */
-    const activeTracks = new Array(count).fill(false);
-
-    activeAttribute
-      .split(',')
-      .map((el) => {
-        return parseInt(el.trim(), 10);
-      })
-      .forEach((trackIndex) => {
-        if (activeTracks[trackIndex] != null) activeTracks[trackIndex] = true;
-      });
-    return activeTracks;
-  }
-
-  async init() {
-    let trackElement = (
-      document.getElementById('template-tracks') as HTMLTemplateElement
-    ).content.cloneNode(true) as DocumentFragment;
-
-    // @ts-ignore
-    this.shadowRoot.innerHTML = '';
-    this.shadowRoot?.append(trackElement);
-
-    this.renderList();
-    this.render();
-  }
-
-  static get observedAttributes() {
-    return ['active', 'count', 'disabled'];
-  }
-  updateStateCount(newCount: number) {
-    this.state.count = newCount;
-    this.renderList();
-    this.render();
-  }
-  updateStateActive(newActive: boolean[]) {
-    this.state.active = [...newActive];
-    this.render();
-  }
-  updateStateDisabled(newValue: boolean) {
-    this.state.disabled = newValue;
-    this.render();
-  }
-
-  renderList() {
-    const { count, active } = this.state;
-    const elList = this.shadowRoot?.getElementById('list');
-    if (elList) {
-      elList.innerHTML = '';
-      for (let i = 0; i < count; i++) {
-        const elLi = document.createElement('li');
-        const elLabel = document.createElement('label');
-        const elInput = document.createElement('input');
-        elInput.type = 'checkbox';
-        if (active[i]) {
-          elInput.checked = true;
-        }
-        elInput.addEventListener('input', (e) => {
-          if (this.state.disabled) {
-            return;
-          }
-          // @ts-ignore
-          const newChecked = e.target?.checked;
-          const newStateActive = [...this.state.active];
-          newStateActive[i] = newChecked;
-          this.updateStateActive(newStateActive);
-
-          this.dispatchEvent(
-            new CustomEvent('tracksActiveChange', {
-              detail: {
-                active: newStateActive,
-              },
-            })
-          );
-        });
-
-        elLabel.appendChild(elInput);
-        elLabel.appendChild(document.createTextNode(` Track ${i + 1}`));
-        elLi.appendChild(elLabel);
-        elList.appendChild(elLi);
-      }
+  static styles = css` 
+    .hidden {
+      display: none;
     }
-  }
+    #list {
+      list-style: none;
+      padding-left: 0;
+      margin-top: 6px;
+      user-select: none;
+    }
+    input[type="checkbox"] {
+      appearance: none;
+      position: relative;
+      background: var(--primary-lightest-2);
+      border-radius: 2px;
+      padding: 2px;
+      margin: 0;
+
+      width: 15px;
+      height: 15px;
+      display: inline-block;
+      vertical-align: middle;
+      top: -1px;
+    }
+    input[type="checkbox"]:checked:after {
+      content: "\u2714";
+      position: absolute;
+      left: 2px;
+      top: 0;
+
+      font-size: 12px;
+      line-height: 13px;
+      color: var(--primary-darker);
+    }
+  `;
 
   render() {
-    const { count, active, disabled } = this.state;
-
-    const elContainer = this.shadowRoot?.getElementById('container');
-    if (elContainer) {
-      if (count === 1 || disabled) {
-        elContainer.classList.add('hidden');
-      } else {
-        elContainer.classList.remove('hidden');
-      }
-    }
-    const elInputs = this.shadowRoot?.querySelectorAll('input');
-    if (elInputs) {
-      elInputs.forEach((elInput, i) => {
-        elInput.checked = active[i];
-      });
-    }
-  }
-
-  attributeChangedCallback(name: string, oldValue: string, newValue: string) {
-    if (oldValue != newValue) {
-      if (name === 'count') {
-        this.updateStateCount(parseFloat(newValue));
-      } else if (name === 'active') {
-        this.updateStateActive(
-          this.parseActiveAttribute(newValue, this.state.count)
-        );
-      } else if (name === 'disabled') {
-        this.updateStateDisabled(newValue != null);
-      }
-    }
+    return html`
+      <div
+        id="container"
+        class=${classMap({
+          hidden: this.count === 1 || this.disabled,
+        })}
+      >
+        Active tracks:
+        <ol id="list">
+          ${Array(this.count)
+            .fill(0)
+            .map((_, i) => {
+              return html`<li>
+                <label>
+                  <input
+                    type="checkbox"
+                    checked="${this.active[i]}"
+                    @input=${(e: InputEvent) => {
+                      const newChecked = (e.target as HTMLInputElement).checked;
+                      const newStateActive = [...this.active];
+                      newStateActive[i] = newChecked;
+                      this.active = newStateActive;
+                      this.dispatchEvent(
+                        new CustomEvent('tracksActiveChange', {
+                          detail: {
+                            active: newStateActive,
+                          },
+                        })
+                      );
+                    }}
+                  />
+                  Track ${i + 1}
+                </label>
+              </li>`;
+            })}
+        </ol>
+      </div>
+    `;
   }
 }
 
-customElements.define('controls-tracks', ControlsTracks);
+declare global {
+  interface HTMLElementTagNameMap {
+    'controls-tracks': ControlsTracks;
+  }
+}
