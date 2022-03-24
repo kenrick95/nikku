e79938ca64bca2c98fd1bd6f778445648b0d9af6
Kenrick <kenrick95@gmail.com>
2022-03-24 20:03:53 +0800

    feat(time-display): rewrite using lit

--- app/src/controls/controls-time-display.ts
+++ app/src/controls/controls-time-display.ts
@@ -1,88 +1,50 @@
+import { html, css, LitElement } from 'lit';
+import { customElement, property } from 'lit/decorators.js';
 
-export class ControlsTimeDisplay extends HTMLElement {
-  state: {
-    value: number;
-    max: number;
-    /** whether value is changable or not*/
-    disabled: boolean;
-  };
-  constructor() {
-    super();
-    this.attachShadow({ mode: 'open' });
-    this.state = {
-      value: parseInt(this.getAttribute('value') || '0', 10) || 0,
-      max: parseInt(this.getAttribute('max') || '0', 10) || 0,
-      disabled: this.getAttribute('disabled') != null,
-    };
+@customElement('controls-time-display')
+export class ControlsTimeDisplay extends LitElement {
+  /** Whether value is changable or not */
+  @property({ type: Boolean }) disabled: boolean = false;
+  /** Current time in seconds */
+  @property({ type: Number }) value: number = 0;
+  /** Max time in seconds */
+  @property({ type: Number }) max: number = 0;
 
-    this.init();
-  }
-
-  async init() {
-    let timeDisplayElement = (
-      document.getElementById('template-time-display') as HTMLTemplateElement
-    ).content.cloneNode(true) as DocumentFragment;
-
-    // @ts-ignore
-    this.shadowRoot.innerHTML = '';
-    this.shadowRoot?.append(timeDisplayElement);
-
-    this.render();
-  }
-
-  static get observedAttributes() {
-    return ['max', 'value', 'disabled'];
-  }
-
-  /**
-   * @param {number} newMax
-   */
-  updateStateMax(newMax: number) {
-    this.state.max = newMax;
-    this.render();
-  }
-  /**
-   * @param {number} newValue
-   */
-  updateStateValue(newValue: number) {
-    this.state.value = newValue;
-    this.render();
-  }
-  /**
-   *
-   * @param {boolean} newValue
-   */
-  updateStateDisabled(newValue: boolean) {
-    this.state.disabled = newValue;
-    this.render();
-  }
-
-  render() {
-    const elCurrentTime = this.shadowRoot?.getElementById('current');
-    const elTotalTime = this.shadowRoot?.getElementById('total');
-
-    if (elCurrentTime) {
-      elCurrentTime.textContent = getFormattedTime(this.state.value);
+  static styles = css`
+    .progress-time-display {
+      display: flex;
+      justify-content: flex-end;
     }
-    if (elTotalTime) {
-      elTotalTime.textContent = getFormattedTime(this.state.max);
+    .separator {
+      margin-inline-start: 4px;
+      margin-inline-end: 4px;
     }
-  }
- 
-  attributeChangedCallback(
-    name: string,
-    oldValue: string | null,
-    newValue: string | null
-  ) {
-    if (oldValue != null && oldValue != newValue) {
-      if (name === 'max') {
-        this.updateStateMax(parseFloat(newValue || ''));
-      } else if (name === 'value') {
-        this.updateStateValue(parseFloat(newValue || ''));
-      } else if (name === 'disabled') {
-        this.updateStateDisabled(newValue != null);
+    .time {
+      width: 3em;
+      text-align: center;
+    }
+    @media (max-width: 640px) {
+      .progress-time-display {
+        justify-content: flex-start;
+      }
+      .time {
+        text-align: left;
       }
     }
+  `;
+
+  render() {
+    return html` <div class="progress-time-display">
+      <div class="time" id="current">${getFormattedTime(this.value)}</div>
+      <div class="separator">/</div>
+      <div class="time" id="total">${getFormattedTime(this.max)}</div>
+    </div>`;
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'controls-time-display': ControlsTimeDisplay;
   }
 }
 
@@ -106,4 +68,3 @@ function getTwoDigits(number: number) {
   }
   return number;
 }
-customElements.define('controls-time-display', ControlsTimeDisplay);
