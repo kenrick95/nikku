efe391092d325016c05d9b2609e4485e38cc69dd
Kenrick <kenrick95@gmail.com>
2022-03-25 23:13:21 +0800

    feat: production build

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,10 +1,4 @@
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  build: {
-    rollupOptions: {
-      external: /^lit/,
-    },
-  },
-});
+export default defineConfig({});
