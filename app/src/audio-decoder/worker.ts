dbdb0b50441be5a27354e0f7484c330aa53a19ca
Kenrick <kenrick95@gmail.com>
2022-03-27 22:51:28 +0800

    feat: decode first 3s first before decoding the rest

--- app/src/audio-decoder/worker.ts
+++ app/src/audio-decoder/worker.ts
@@ -1,14 +1,41 @@
-import { Brstm } from 'brstm';
+import { Brstm, Metadata } from 'brstm';
 import { transfer } from 'comlink';
 
-export function decode(buffer: ArrayBuffer) {
-  const brstm = new Brstm(buffer);
+let buffer: ArrayBuffer | null = null;
+let brstm: Brstm | null = null;
+export function init(receivedBuffer: ArrayBuffer) {
+  buffer = receivedBuffer;
+  brstm = new Brstm(receivedBuffer);
+}
+export function destroy() {
+  buffer = null;
+  brstm = null;
+}
+export function getMetadata(): Metadata | undefined {
+  if (!brstm) {
+    return;
+  }
+  return brstm.metadata;
+}
+
+export function getAllSamples() {
+  if (!brstm) {
+    return;
+  }
   const allSamples = brstm.getAllSamples();
-  return {
-    metadata: brstm.metadata,
-    allSamples: transfer(
-      allSamples,
-      allSamples.map((allSamplesPerChannel) => allSamplesPerChannel.buffer)
-    ),
-  };
+  return transfer(
+    allSamples,
+    allSamples.map((allSamplesPerChannel) => allSamplesPerChannel.buffer)
+  );
+}
+
+export function getSamples(offset: number, size: number) {
+  if (!brstm) {
+    return;
+  }
+  const allSamples = brstm.getSamples(offset, size);
+  return transfer(
+    allSamples,
+    allSamples.map((allSamplesPerChannel) => allSamplesPerChannel.buffer)
+  );
 }
