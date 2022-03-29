868ca42e0791ecab249f935cd1eeb6e6c30a048f
Kenrick <kenrick95@gmail.com>
2022-03-29 23:24:52 +0800

    feat: initial version of audio source node

--- app/src/audio-player/audio-source.js
+++ app/src/audio-player/audio-source.js
@@ -5,7 +5,7 @@
 /**
  * Purpose:
  * - All-in-one source node
- * 
+ *
  * Requirements:
  * - Buffer can be dynamically updated
  *   - Use case: Decode first block, then write here, and can be used for playing already! Afterwards, next blocks can be written and no need to disconnect/reconnect the node
@@ -19,16 +19,59 @@ class AudioSourceNode extends AudioWorkletProcessor {
    */
   constructor(options) {
     super();
-    this.port.onmessage = (event) => {};
-  }
+    /** @type {Float32Array[]} */
+    this.initialSamples = options?.processorOptions?.initialSamples;
+    /** @type {null | number} */
+    this.subsequentSamplesOffset = null;
+    /** @type {Float32Array[]} */
+    this.subsequentSamples = [];
 
+    this._bufferHead = 0;
+
+    this.port.onmessage = (event) => {
+      if (event.data.type === 'update-samples') {
+        const samples = /** @type {Float32Array[]} */ (
+          event.data.payload.samples
+        );
+        const offset = /** @type {number} */ (event.data.payload.offset);
+        this.subsequentSamples = samples;
+        this.subsequentSamplesOffset = offset;
+      }
+    };
+  }
   /**
    *
-   * @param {Array<Array<Float32Array>>} inputs
+   * @param {Array<Array<Float32Array>>} _inputs
    * @param {Array<Array<Float32Array>>} outputs
    * @param {Object} _parameters
+   * @returns {boolean}
    */
-  process(inputs, outputs, _parameters) {
+  process(_inputs, outputs, _parameters) {
+    const output = outputs[0];
+    if (!output || !output.length || !output[0].length) {
+      return false;
+    }
+    const offset = this._bufferHead;
+    for (let c = 0; c < output.length; c++) {
+      for (let s = 0; s < output[c].length; s++) {
+        if (s + offset <= this.initialSamples[c].length) {
+          output[c][s] = this.initialSamples[c][s + offset];
+        } else if (
+          this.subsequentSamplesOffset != null &&
+          s + offset > this.subsequentSamplesOffset &&
+          s + offset <=
+            this.subsequentSamples[c].length + this.subsequentSamplesOffset
+        ) {
+          output[c][s] =
+            this.subsequentSamples[c][
+              s + offset - this.subsequentSamplesOffset
+            ];
+        } else {
+          return false;
+        }
+      }
+    }
+    this._bufferHead += output[0].length;
     return true;
   }
 }
