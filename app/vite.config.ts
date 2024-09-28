9c27a5824dae90b6d77f2c0b93f604d56fc87406
Kenrick <kenrick95@gmail.com>
2024-09-28 22:13:15 +0800

    feat: upgrade dependencies

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -20,31 +20,28 @@ export default defineConfig({
           {
             src: './assets/android-launchericon-192-192.png',
             sizes: '192x192',
-            type: 'image/png'
+            type: 'image/png',
           },
           {
             src: './assets/android-launchericon-512-512.png',
             sizes: '512x512',
-            type: 'image/png'
-          }
-        ]
+            type: 'image/png',
+          },
+        ],
       },
       includeAssets: ['./.well-known/assetlinks.json'],
     }),
   ],
   worker: {
-    plugins: [comlink()],
+    plugins: () => [comlink()],
   },
   server: {
     watch: {
-      ignored: ['!**/node_modules/brstm/**']
-    }
+      ignored: ['!**/node_modules/brstm/**'],
+    },
   },
   optimizeDeps: {
-    exclude: ['brstm']
-  },
-  resolve: {
-    conditions: ['nikku:source']
+    exclude: ['brstm'],
   },
   // build: {
   //   rollupOptions: {
